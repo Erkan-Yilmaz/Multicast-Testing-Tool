c8ed3809dec5a8a6f50f078f733a94b394d07d45
dave <none@none>
2011-04-29 09:03:58 +0200

    LanguageChange Manager+Listener hinzugefügt, exitApplication hinzugefügt

---
+++ src/main/java/com/spam/mctool/controller/LanguageManager.java
@@ -0,0 +1,31 @@
+package com.spam.mctool.controller;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * This interface is implemented by the controller and provides the functionality to
+ * change the global language and inform all listeners
+ * @author David Hildenbrand
+ */
+public interface LanguageManager {
+
+    /**
+     * Add an listener for the languageChange events.
+     * @param l listener to be added
+     */
+    public void addLanguageChangeListener(com.spam.mctool.controller.LanguageChangeListener l);
+
+    /**
+     * Remove a listener from the list of listeners for the languageChange event.
+     * @param l listener to be removed
+     */
+    public void removeProfileChangeListener(com.spam.mctool.controller.LanguageChangeListener l);
+
+    /**
+     * Inform all observers about a language change
+     */
+    public void reportLanguageChange();
+}
